ce0b7aad01e40f291c4fa51858e02bd31e4b4b17
Tru Nguyen <nguyentruhn@gmail.com>
2023-11-26 18:00:42 +0700

    timeline

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+// menu
 function scrollingMenu(menuClass) {
   const sectionMenu = document.querySelector(menuClass); // biến lưu trữ phần tử có class đc truyền vào
   window.scrollTo({
@@ -7,21 +8,18 @@ function scrollingMenu(menuClass) {
   });
 }
 
+// nut back top
 function backToTop() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
+// show nút back top
 var lastScrollTop = 0;
-
 window.addEventListener("scroll", handleScroll);
 
 function handleScroll() {
-  var backToTopButton = document.querySelector(".back-to-top");
-
-  // Lấy vị trí cuộn hiện tại
-  var scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-  // Kiểm tra nếu vị trí của trang lớn hơn 20 pixel và trang đang cuộn hoặc đứng im
+  var backToTopButton = document.querySelector(".back-to-top"); // Lấy vị trí cuộn hiện tại
+  var scrollTop = window.scrollY || document.documentElement.scrollTop; // Kiểm tra nếu vị trí của trang lớn hơn 20 pixel và trang đang cuộn hoặc đứng im
   if ((scrollTop > 20 || lastScrollTop === 0) && scrollTop > lastScrollTop) {
     backToTopButton.style.display = "block";
   } else {
@@ -32,7 +30,6 @@ function handleScroll() {
   lastScrollTop = scrollTop;
 }
 
-
 // animate scroll
 function timeline() {
   var timelines = document.querySelectorAll(".timeline-box");
@@ -40,12 +37,16 @@ function timeline() {
     var windowHeight = window.innerHeight; // chieu cao cua window
     var elementTop = timelines[i].getBoundingClientRect().top;
     var elementVisible = 150;
-    console.log(elementTop, windowHeight, elementVisible, elementTop < windowHeight - elementVisible,timelines[i]);
+    // console.log(
+    //   elementTop,
+    //   windowHeight,
+    //   elementVisible,
+    //   elementTop < windowHeight - elementVisible,
+    //   timelines[i]
+    // );
 
     if (elementTop < windowHeight - elementVisible) {
       timelines[i].classList.add("active");
-    } else {
-      // timelines[i].classList.remove("active");
     }
   }
 }
@@ -54,9 +55,21 @@ window.addEventListener("scroll", timeline);
 
 function toggleSidebar() {
   var element = document.getElementById("sidebar");
-  if (element.classList.contains('toogle')) {
+  if (element.classList.contains("toogle")) {
     element.classList.remove("toogle");
   } else {
     element.classList.add("toogle");
   }
+}
+
+// xem thêm
+
+function seeMore() {
+  var dot = document.getElementById("dot");
+  var paragraph = document.querySelector(".paragraph");
+  var buttonHome = document.getElementById("button-home");
+  
+      paragraph.classList.remove("paragraph");
+      dot.style.display = "none";
+      buttonHome.style.display = "none";
 }
